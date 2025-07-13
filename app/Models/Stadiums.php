adfefe38276c047e6726123384aa931db1e54733
Walter Developer <47099208+WalterDeveloper@users.noreply.github.com>
2025-07-13 18:14:34 +0200

    Update Stadiums.php

--- app/Models/Stadiums.php
+++ app/Models/Stadiums.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+
 class Stadiums extends Model
 {
     // use HasFactory;
